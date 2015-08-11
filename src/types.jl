698acf3b3a4e426a7a857d98fcaffc6ff3e9c34d
Will Kearney <wkearn@gmail.com>
2015-08-11 11:55:44 -0400

    Remove some duplicated type definitions from src/types.jl
    The DiffMap type and show functionality are already in src/diffmaps.jl

--- src/types.jl
+++ src/types.jl
@@ -3,24 +3,3 @@ abstract AbstractResult
 abstract SpectralResult <: AbstractResult
 
 typealias Projection Matrix{Float64}
-
-type Diffmap
-    d::Int
-    t::Int
-    K::Matrix{Float64}
-    Y::Matrix{Float64}
-end
-
-function Base.show(io::IO, res::Diffmap)
-    println(io, "Dimensionality:")
-    show(io, res.d)
-    print(io, "\n\n")
-    println(io, "Timesteps:")
-    show(io, res.t)
-    print(io, "\n\n")
-    println(io, "Kernel:")
-    show(io, res.K)
-    print(io, "\n\n")
-    println(io, "Embedding:")
-    show(io, res.Y)
-end
