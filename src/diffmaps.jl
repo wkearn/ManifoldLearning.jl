5ed713bc18af56d71327e9def98f6c8687a649aa
Will Kearney <wkearn@gmail.com>
2015-08-13 12:01:05 -0400

    Add the eigenvalue storage into the DiffMap type
    This also changes a bunch of epsilons which weren't matching with
    my Unicode epsilons. That's probably going to be a pain in the ass.
    
    Also a few corrections to the Nystrom method because we don't multiply
    by the eigenvalues for the diffusion coordinates

--- src/diffmaps.jl
+++ src/diffmaps.jl
@@ -6,11 +6,12 @@
 #### DiffMap type
 immutable DiffMap <: SpectralResult
     t::Int
-    ɛ::Float64
+    ε::Float64
+    λ::Vector{Float64}
     K::Matrix{Float64}
     proj::Projection
 
-    DiffMap(t::Int, ɛ::Float64, K::Matrix{Float64}, proj::Projection) = new(t, ɛ, K, proj)
+    DiffMap(t::Int, ε::Float64, λ::Vector{Float64}, K::Matrix{Float64}, proj::Projection) = new(t, ε, λ, K, proj)
 end
 
 ## properties
@@ -20,7 +21,7 @@ kernel(M::DiffMap) = M.K
 
 ## show & dump
 function show(io::IO, M::DiffMap)
-    print(io, "Diffusion Maps(outdim = $(outdim(M)), t = $(M.t), ɛ = $(M.ɛ))")
+    print(io, "Diffusion Maps(outdim = $(outdim(M)), t = $(M.t), ε = $(M.ε))")
 end
 
 function dump(io::IO, M::DiffMap)
@@ -30,6 +31,8 @@ function dump(io::IO, M::DiffMap)
     println(io)
     println(io, "projection:")
     Base.showarray(io, projection(M), header=false, repr=false)
+    println(io, "eigenvalues:")
+    Base.showarray(io,M.λ,header=false,repr=false)
 end
 
 ## interface functions
@@ -49,12 +52,13 @@ function transform(::Type{DiffMap}, X::DenseMatrix{Float64}; d::Int=2, α::Int=1
     U ./= U[:,1]
     Y = U[:,2:end]
 
-    return DiffMap(α, ε, K, Y')
+    return DiffMap(α, ε, s[2:end], K, Y')
 end
 
 # Nystrom extension for regression taken from Freeman et al. MNRAS 2009
 function nystrom(dm::DiffMap, X::DenseMatrix{Float64}, Y::DenseMatrix{Float64})
     W = exp(-pairwise(SqEuclidean(),X,Y)./dm.ε) #Compute the kernel
-    W./sum(W,1) # Row-normalize except our rows are columns
-    diagm(1./dm.λ)*dm.proj*W #Eq. 4 from Freeman but reversed because everything is transposed
+    W./=sum(W,1) # Row-normalize except our rows are columns
+    dm.proj*W #Eq. 4 from Freeman but reversed because everything is transposed
+    # and we don't multiply by the eigenvalues in the projection matrix
 end
